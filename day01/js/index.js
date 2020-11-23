3eb64b6f629ecc103190fedb0f0bef5b219c762e
www <liboshi@sin.com>
2020-11-23 21:59:20 +0800

    二次提交

--- day01/js/index.js
+++ day01/js/index.js
@@ -1,17 +1,51 @@
 var confirm = document.getElementById('confirm');
-var name_list = document.getElementById('name_list');
-var arr_name = [];
+var nameList = document.getElementById('name_list');
+var deleteLi = document.getElementById('deleteLi');
+var search = document.getElementById('search');
+var arrName = [];
 console.log(confirm)
-confirm.onclick = function(){
-    var input_value = document.getElementById('input_value').value;
-    if(input_value == ""){
-        alert('不能为空')
-    }else{
-        var li = document.createElement('li');
-        name_list.appendChild(li);
-        li.innerText = input_value;
-        arr_name.push(input_value);
-        console.log(arr_name);
-        input_value.value = '';
-    }
+function render(){
+    nameList.innerHTML="";
+  for (let i = 0; i < arrName.length; i++) {
+    var li = document.createElement("li");
+    console.log(li)
+    li.innerText = arrName[i];
+    nameList.appendChild(li);
+  }
 }
+confirm.onclick = function(){
+  var input_value = document.getElementById('input_value').value;
+  arrName.push(input_value);
+  console.log(arrName);
+  render()
+}
+deleteLi.onclick = function(){
+    arrName.splice(0,1)
+    render()
+  }
+  //查 有的话只包含，否则为空。
+search.onclick = function(){
+  var input_value = document.getElementById('input_value').value;
+  console.log(input_value);
+  console.log(arrName);
+  function checkAdult(arr) {
+      return arr == input_value;
+    };
+  arrName.filter(checkAdult)
+  console.log(arrName.filter(checkAdult))
+  var arr_name_search = (arrName.filter(checkAdult));
+  nameList.innerHTML="";
+  for (let i = 0; i < arr_name_search.length; i++) {
+    var li = document.createElement("li");
+    console.log(li)
+    li.innerText = arr_name_search[i];
+    nameList.appendChild(li);
+  }
+}
+
+
+// var ages = ['c','z','a'];
+// function checkAdult(age) {
+//     return age == "c";
+// }
+// console.log(ages.filter(checkAdult))
