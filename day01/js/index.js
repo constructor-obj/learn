c0b8b74747ced7fb934090625b6e32d5101c719c
www <liboshi@sin.com>
2020-11-25 20:59:47 +0800

    修改render函数参数问题

--- day01/js/index.js
+++ day01/js/index.js
@@ -1,51 +1,40 @@
-var confirm = document.getElementById('confirm');
-var nameList = document.getElementById('name_list');
-var deleteLi = document.getElementById('deleteLi');
-var search = document.getElementById('search');
+var confirm = document.getElementById("confirm");
+var nameList = document.getElementById("name_list");
+var deleteLi = document.getElementById("deleteLi");
+var search = document.getElementById("search");
 var arrName = [];
-console.log(confirm)
-function render(){
-    nameList.innerHTML="";
-  for (let i = 0; i < arrName.length; i++) {
+console.log(confirm);
+function render(parament) {
+  nameList.innerHTML = "";
+  for (let i = 0; i < parament.length; i++) {
     var li = document.createElement("li");
-    console.log(li)
-    li.innerText = arrName[i];
+    console.log(li);
+    li.innerText = parament[i];
     nameList.appendChild(li);
   }
 }
-confirm.onclick = function(){
-  var input_value = document.getElementById('input_value').value;
+confirm.onclick = function () {
+  var input_value = document.getElementById("input_value").value;
   arrName.push(input_value);
   console.log(arrName);
-  render()
-}
-deleteLi.onclick = function(){
-    arrName.splice(0,1)
-    render()
-  }
-  //查 有的话只包含，否则为空。
-search.onclick = function(){
-  var input_value = document.getElementById('input_value').value;
-  console.log(input_value);
-  console.log(arrName);
+  render(arrName);
+};
+deleteLi.onclick = function () {
+  arrName.splice(0, 1);
+  render(arrName);
+};
+//查 有的话只包含，否则为空。
+search.onclick = function () {
+  var inputValue = document.getElementById("input_value").value;
   function checkAdult(arr) {
-      return arr == input_value;
-    };
-  arrName.filter(checkAdult)
-  console.log(arrName.filter(checkAdult))
-  var arr_name_search = (arrName.filter(checkAdult));
-  nameList.innerHTML="";
-  for (let i = 0; i < arr_name_search.length; i++) {
-    var li = document.createElement("li");
-    console.log(li)
-    li.innerText = arr_name_search[i];
-    nameList.appendChild(li);
+    return arr == inputValue;
   }
+  arrName.filter(checkAdult);
+  var arrNameSearch = arrName.filter(checkAdult);
+  render(inputValue.split(","));
+};
+
+function greet(xm) {
+  console.log(xm);
 }
-
-
-// var ages = ['c','z','a'];
-// function checkAdult(age) {
-//     return age == "c";
-// }
-// console.log(ages.filter(checkAdult))
+greet("wangweiwei");
