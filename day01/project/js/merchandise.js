094b0d94ecb3eb33260959db4fb23c4d0db80701
www <liboshi@sin.com>
2020-12-01 19:14:55 +0800

    整理css代码

---
+++ day01/project/js/merchandise.js
@@ -0,0 +1,11 @@
+var goWuChe = document.getElementsByClassName("gouwuche")[0];
+console.log(goWuChe);
+var qqq = document.getElementById("qqq");
+console.log(qqq);
+var ccc = document.querySelector(".seckill-item__price .gouwuche");
+console.log(ccc);
+var ooo = document.querySelectorAll(".seckill-item__price .gouwuche");
+console.log(ooo);
+qqq.onclick = function () {
+  alert(1);
+};
